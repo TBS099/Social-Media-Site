6161a7238ce2583e9e176a32b7026f1995037137
Tanush Bikram Shah <109902776+TBS099@users.noreply.github.com>
2024-11-05 17:02:50 +0000

    Added Throbber (Loading Icon) and removed unnecessary server calls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5000; // Using port 5000
+const port = process.env.PORT || 5000; // Using port 5000
 const STUDENT_ID = 'YOUR-ID'; // Student ID, just for requirement, use home if needed
 
 // Redirect from the root URL to ID
@@ -13,12 +13,7 @@ app.get('/', (req, res) => {
 // Middleware to serve static files from the 'public' directory under ID
 app.use(`/${STUDENT_ID}`, express.static('public'));
 
-// Basic route for the home page
-app.get(`/${STUDENT_ID}/`, (req, res) => {
-    res.send('Hello, world! This is my web app.');
-});
-
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/${STUDENT_ID}/`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/${STUDENT_ID}/`);
 });

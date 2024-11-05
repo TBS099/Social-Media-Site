12ba0fed1369530493969641f2fdfe91ed61e8fc
Tanush Bikram Shah <109902776+TBS099@users.noreply.github.com>
2024-11-05 20:10:22 +0000

    Added Post, Updated theme toggle
    Added template for post and kept in JS file for now.
    Made minor edit on theme toggler to make it better.
    Added like counting function with space for future improvements

--- public/src/js/site.js
+++ public/src/js/site.js
@@ -1,3 +1,5 @@
+
+//Function to toggle between light and dark theme
 $(document).ready(function () {
     const themeToggle = $('[data-theme-toggle]');
 
@@ -5,10 +7,14 @@ $(document).ready(function () {
     function setTheme(theme) {
         $('html').attr('data-theme', theme);
         localStorage.setItem('theme', theme);
+
+        if (theme === 'light') {
+            themeToggle.prop('checked', true);
+        }
     }
 
     // Load the saved theme from localStorage
-    const savedTheme = localStorage.getItem('theme') || 'dark';
+    const savedTheme = localStorage.getItem('theme') || 'light';
     setTheme(savedTheme);
 
     // Event listener for theme toggle switch
@@ -20,3 +26,47 @@ $(document).ready(function () {
         }
     });
 });
+
+// Function to handle the like click
+function handle_post_like(postId) {
+    const icon = document.getElementById(`${postId}`);
+    const count = document.getElementById(`count-${postId}`);
+
+    // Parse the current count and increment it
+    let current_count = parseInt(count.textContent);
+    current_count += 1;
+
+    // Update the like count in the <p> element
+    count.textContent = current_count;
+
+    // Log a message to the console
+    console.log(`Post ${postId} liked! Total likes: ${current_count}`);
+}
+
+// Function to create a post on the basis of the provided data
+function create_post(post) {
+    const post_template = `
+    <div class="post-container">
+        <div class="post">
+            <div class="post-header">
+                <img src="{post.profilePicture}" alt="Profile Picture" class="profile-picture">
+                <p class="author-name">${post.author}</p>
+                <p class="post-date">${post.date}</p>
+            </div>
+            <p class="post-content">${post.content}</p>
+            <div class="post-footer">
+                <div class="like">
+                    <i class="far fa-heart" id="${post.ID}" onclick="handle_post_like(${post.ID})"></i>
+                    <p class="like-count" id="count-${post.ID}">${post.like}</p>
+                </div>
+                <div class="comment">
+                    <i class="far fa-comment"></i>
+                    <p class="comment-count">${post.comment}</p>
+                </div>
+            </div>
+        </div>
+    </div>`
+
+    return post_template;
+}
+
